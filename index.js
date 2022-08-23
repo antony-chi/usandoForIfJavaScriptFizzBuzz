296bd84586d48cb9a0903a3ad15dfad669e75458
antony-chi <antony.cp9@gmail.com>
2022-08-23 00:09:10 -0500

    este e

---
+++ index.js
@@ -0,0 +1,14 @@
+for (let index = 1; index <= 100; index++) {
+    if(index % 3 === 0 && index % 5 === 0){
+        console.log(index + " FizzBuzz")
+    }
+    else if(index % 3 === 0){
+        console.log(index + " Fizz")
+    }
+    else if(index % 5 === 0){
+        console.log( index + " Buzz")
+    }
+    else{
+        console.log(index)
+    }
+}
